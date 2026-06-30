2a7d2fc44e64d656cd6163836a22c7214d5135d5
Jakub Gerwel <kuba.gerwel@gmail.com>
2026-06-30 19:58:12 +0200

    init

---
+++ backend/match-movie/match-movie/src/main/java/io/github/jacob66g/matchmovie/MatchMovieApplication.java
@@ -0,0 +1,13 @@
+package io.github.jacob66g.matchmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchMovieApplication.class, args);
+	}
+
+}
---
+++ backend/match-movie/match-movie/src/test/java/io/github/jacob66g/matchmovie/MatchMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jacob66g.matchmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
